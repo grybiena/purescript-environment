24d04ac866ff6c20345cbdfa038704b4f93e9fe2
f <f@grybiena.com>
2023-06-07 21:24:37 +0200

    templates and common dependencies for purescript projects

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 {
   description = "purescript-environment";
   inputs = {
-    get-flake.url = "github:ursi/get-flake";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     ps-tools.follows = "purs-nix/ps-tools";
@@ -11,9 +10,61 @@
         url = "github:grybiena/npmlock2nix?ref=grybiena";
       };
   };
-  outputs = { flake-utils, ps-tools, ...} :
-    flake-utils.lib.eachDefaultSystem (system: {
-      packages.default = ps-tools.legacyPackages.${system}.purescript;
-    });
-}
+  outputs = inputs:
+    with inputs;
+    rec {
+      inherit nixpkgs flake-utils ps-tools purs-nix npmlock2nix;
+      gen-overlays = {
+        __functor = _: { pkgs, system }: overlays:
+        let overlayInput = name: input:
+              if input ? packages
+                then let pkg = input.packages.${system}.default;
+                     in if pkg ? purs-nix-info then { "${name}" = pkg; } else {}
+                else {};
+          in [ (self: super: pkgs.lib.attrsets.concatMapAttrs overlayInput overlays) ];
+      };
+      build-package = {
+        __functor = _: build@{ system, name, src, overlays, derive-package, shellHook ? "", ... }:
+          let
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = [ ];
+              config.allowBroken = true;
+            };
+            purs-nix-overlay = purs-nix {
+              inherit system; 
+              overlays = with inputs; gen-overlays { inherit pkgs system; } overlays;
+            };
 
+            package = import derive-package (purs-nix-overlay // {
+              npmlock2nix = import npmlock2nix { inherit pkgs; };
+            }); 
+
+            ps = purs-nix-overlay.purs { inherit (package) dependencies; };
+
+
+            shell = pkgs.mkShell
+              { inherit shellHook;
+                packages = with pkgs; [
+                    nodejs
+                    (ps.command {}) 
+                    ps-tools.legacyPackages.${system}.for-0_15.purescript-language-server
+                    purs-nix-overlay.esbuild
+                    purs-nix-overlay.purescript
+                  ];
+              };
+          in { packages = {
+                 default =
+                   purs-nix-overlay.build
+                     { inherit name; 
+                       src.path = src;
+                       info = package;
+                     };
+                 output = ps.output {};
+
+               } // (if build ? bundle then { bundle = ps.bundle build.bundle; } else {});
+               devShells.default = shell; 
+             };
+      };
+    };
+}
