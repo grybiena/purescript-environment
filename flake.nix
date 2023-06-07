192dd4bc90c4fb346617696a1bfea62f795c5830
f <f@grybiena.com>
2023-06-08 01:35:33 +0200

    brackets

--- flake.nix
+++ flake.nix
@@ -33,7 +33,9 @@
               overlays = with inputs; gen-overlays { inherit pkgs system; } overlays;
             };
 
-            package = import derive-package (purs-nix-overlay // { npmlock2nix = import npmlock2nix { inherit pkgs}; } ); 
+            package = import derive-package (purs-nix-overlay // {
+              npmlock2nix = import npmlock2nix { inherit pkgs; };
+            }); 
 
             ps = purs-nix-overlay.purs { inherit (package) dependencies; };
 
