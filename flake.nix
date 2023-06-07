e491f0335af85ec9d0f6d4ddd2f181ac10d7642f
f <f@grybiena.com>
2023-06-07 22:51:47 +0200

    overlay templating

--- flake.nix
+++ flake.nix
@@ -1,88 +1,10 @@
 {
   description = "purescript-environment";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    ps-tools.follows = "purs-nix/ps-tools";
-    purs-nix.url = "github:grybiena/purs-nix?ref=grybiena";
-    npmlock2nix =
-      { flake = false;
-        url = "github:grybiena/npmlock2nix?ref=grybiena";
-      };
-
-    crypto-secp256k1 = {
-      url = "git+ssh://git@github.com/grybiena/crypto-secp256k1?ref=grybiena";
-      inputs = {
-        flake-utils.follows = "flake-utils";
-        nixpkgs.follows     = "nixpkgs";
-        ps-tools.follows    = "ps-tools";
-        purs-nix.follows    = "purs-nix";
-        npmlock2nix.follows = "npmlock2nix";
-
-      };
-    };
- 
-    mycelial-fibration = {
-      url = "git+ssh://git@github.com/grybiena/mycelial-fibration?ref=grybiena";
-      inputs = {
-        flake-utils.follows = "flake-utils";
-        nixpkgs.follows     = "nixpkgs";
-        ps-tools.follows    = "ps-tools";
-        purs-nix.follows    = "purs-nix";
-        npmlock2nix.follows = "npmlock2nix";
-        crypto-secp256k1.follows = "crypto-secp256k1";
-      };
-    };
-
-    mycelial-socket = {
-      url = "git+ssh://git@github.com/grybiena/mycelial-socket?ref=overlays";
-      inputs = {
-        flake-utils.follows = "flake-utils";
-        nixpkgs.follows     = "nixpkgs";
-        ps-tools.follows    = "ps-tools";
-        purs-nix.follows    = "purs-nix";
-      };
-    };
-
-    mycelial-space = {
-      url = "git+ssh://git@github.com/grybiena/mycelial-space?ref=overlays";
-      inputs = {
-        flake-utils.follows = "flake-utils";
-        nixpkgs.follows     = "nixpkgs";
-        ps-tools.follows    = "ps-tools";
-        purs-nix.follows    = "purs-nix";
-      };
-    };
-
-    web-rtc = {
-      url = "git+ssh://git@github.com/grybiena/web-rtc?ref=grybiena";
-      inputs = {
-        flake-utils.follows = "flake-utils";
-        nixpkgs.follows     = "nixpkgs";
-        ps-tools.follows    = "ps-tools";
-        purs-nix.follows    = "purs-nix";
-      };
-    };
-
-
   };
-  outputs = inputs@{ flake-utils, purs-nix, ...} :
-    { __functor = _: { pkgs, system }:
-        purs-nix {
-          inherit system;
-          overlays = let otherInputs = [ "nixpkgs" "flake-utils" "ps-tools" "purs-nix" "npmlock2nix" ];
-                         overlayInput = name: input:
-                           if pkgs.lib.lists.any (other: other == name) otherInputs
-                             then {}
-                             else { "${name}" = input.packages.${system}.default; };
-                     in [ (self: super: pkgs.lib.attrsets.concatMapAttrs overlayInput inputs) ];
-       };
-      nixpkgs     = inputs.nixpkgs;
-      flake-utils = inputs.flake-utils;
-      ps-tools    = inputs.ps-tools;
-      purs-nix    = inputs.purs-nix;
-      npmlock2nix = inputs.npmlock2nix;
+  outputs = _:
+    { __functor = _: { pkgs, system }: overlays:
+        let overlayInput = name: input: { "${name}" = input.packages.${system}.default; };
+        in [ (self: super: pkgs.lib.attrsets.concatMapAttrs overlayInput overlays) ];
     };
 }
-
-
