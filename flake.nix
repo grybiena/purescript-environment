a8c00b46de86afdb8c13f174a87f2a765c468d08
f <f@grybiena.com>
2023-06-07 23:01:33 +0200

    add common deps

--- flake.nix
+++ flake.nix
@@ -1,10 +1,22 @@
 {
   description = "purescript-environment";
   inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    ps-tools.follows = "purs-nix/ps-tools";
+    purs-nix.url = "github:grybiena/purs-nix?ref=grybiena";
+    npmlock2nix =
+      { flake = false;
+        url = "github:grybiena/npmlock2nix?ref=grybiena";
+      };
   };
-  outputs = _:
-    { __functor = _: { pkgs, system }: overlays:
-        let overlayInput = name: input: { "${name}" = input.packages.${system}.default; };
-        in [ (self: super: pkgs.lib.attrsets.concatMapAttrs overlayInput overlays) ];
+  outputs = inputs:
+    with inputs;
+    { inherit nixpkgs flake-utils ps-tools purs-nix npmlock2nix;
+      gen-overlays = {
+        __functor = _: { pkgs, system }: overlays:
+          let overlayInput = name: input: { "${name}" = input.packages.${system}.default; };
+          in [ (self: super: pkgs.lib.attrsets.concatMapAttrs overlayInput overlays) ];
+      };
     };
 }
